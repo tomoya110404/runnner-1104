18e99ca02dc4f4abd39c6ca47bd408a479363c52
tomoya110404 <moonlight.02.1104@gmail.com>
2020-11-23 19:37:58 +0900

    フロント部分　ユーザー

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+         
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+    validates :height
+    validates :weight
+  end  
+
+end
