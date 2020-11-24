3d4519ce854f0b426fe88f65f546cb941c3f62bc
tomoya110404 <moonlight.02.1104@gmail.com>
2020-11-24 16:06:43 +0900

    記事詳細ページ作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,16 @@ class ArticlesController < ApplicationController
     end    
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+
   private
 
   def article_params
