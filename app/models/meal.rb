1bf0cd4beda329910587f3b162776da30f30159b
tomoya110404 <moonlight.02.1104@gmail.com>
2020-12-02 16:24:35 +0900

    バリデーション

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,4 +3,10 @@ class Meal < ApplicationRecord
   has_one_attached :image
   has_many :meal_favorites, dependent: :destroy
   has_many :meals_comments, dependent: :destroy
+
+  with_options presence: true do
+    validates :name
+    validates :kondate
+    validates :recipe
+  end
 end
