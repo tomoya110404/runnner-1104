f16d2a31df42ea27ea4f93664a8a5f8475e11392
tomoya110404 <moonlight.02.1104@gmail.com>
2020-12-06 19:33:20 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,8 @@
 lock '3.14.1'
 
 set :application, 'runnner-1104'
-set :repo_url, 'git@example.com:tomoya110404/runnner-1104.git'
+
+set :repo_url, 'git@github.com:tomoya110404/runnner-1104.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
