7298347ebe02afc0703148f8a8530d48e78bfa22
tomoya110404 <moonlight.02.1104@gmail.com>
2020-12-14 19:12:21 +0900

    開発環境にDocker導入

--- db/schema.rb
+++ db/schema.rb
@@ -98,11 +98,6 @@ ActiveRecord::Schema.define(version: 2020_12_01_131900) do
     t.index ["user_id"], name: "index_meals_comments_on_user_id"
   end
 
-  create_table "runners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
